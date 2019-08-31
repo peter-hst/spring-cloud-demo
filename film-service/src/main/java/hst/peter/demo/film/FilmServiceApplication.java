d2218cf4fb1dcd2759256e5843ce95ef4e341e18
peter-hst <h_st@live.cn>
2019-09-01 07:42:56 +0800

    add restTemplate

--- film-service/src/main/java/hst/peter/demo/film/FilmServiceApplication.java
+++ film-service/src/main/java/hst/peter/demo/film/FilmServiceApplication.java
@@ -3,6 +3,9 @@ package hst.peter.demo.film;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,14 @@ public class FilmServiceApplication {
         SpringApplication.run(FilmServiceApplication.class, args);
     }
 
+
+    /**
+     * ribbon 测试
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
