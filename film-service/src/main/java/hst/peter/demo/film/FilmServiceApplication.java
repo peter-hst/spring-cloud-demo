0ea3263a34be4f91c6d3a861502b255aacd64271
peter-hst <h_st@live.cn>
2019-09-01 21:46:17 +0800

    add hystrix dashboard

--- film-service/src/main/java/hst/peter/demo/film/FilmServiceApplication.java
+++ film-service/src/main/java/hst/peter/demo/film/FilmServiceApplication.java
@@ -1,19 +1,34 @@
 package hst.peter.demo.film;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients // 开启Feign客户端
 @EnableCircuitBreaker // @EnableCircuitBreaker或@EnableHystrix 开启Hystrix断路器
+@EnableHystrixDashboard // 开启Hystrix-Dashboard 可视化监控
 public class FilmServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FilmServiceApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
